18f864e73685466499acccc9dd25ad85b91a3eff
pa13lito <53452935+pa13lito@users.noreply.github.com>
2024-02-23 15:37:46 +0100

    Update script.js
    add new variables

--- comp_1/dist/script.js
+++ comp_1/dist/script.js
@@ -1,17 +1,21 @@
 data = [
   {
   type: 'scatterpolar',
-  r: [39, 80, 80, 50],
-  theta: ["Potencia", "Autonomía", "Velocidad", "Precio"],
+  r: [90, 95, 87, 70, 67, 74, 70, 95],
+  theta: ["Aceleración", "Frenado", "Rendimiento en subidas", "Autonomía", "Precio", "Maniobrabilidad", "Servicio Técnico", "Velocidad"],
   fill: 'toself',
-  name: 'Dualtron Ultra-II 72V 40Ah'
+  name: '<b>Dualtron Ultra-II 72V 40Ah</b>',
+  hovertemplate: "<b>%{theta}</b><br>%{r} Puntos</br>",
+  textposition: "middle left"
   },
   {
   type: 'scatterpolar',
-  r: [24, 74, 89, 90  ],
-  theta: ["Potencia", "Autonomía", "Velocidad", "Precio"],
+  r: [80, 74, 78, 90, 60, 70, 60, 90],
+  theta: ["Aceleración", "Frenado", "Rendimiento en subidas", "Autonomía", "Precio", "Maniobrabilidad", "Servicio Técnico", "Velocidad"],
   fill: 'toself',
-  name: 'NAMI Burn-e 72V'
+  name: '<b>NAMI Burn-e 72V</b>',
+  hovertemplate: "<b>%{theta}</b><br>%{r} Puntos</br>",
+  textposition: "middle left"
   }
 ]
 
@@ -21,7 +25,10 @@ layout = {
       visible: true,
       range: [0, 100]
     }
-  }
+  }, showlegend: true
 }
 
-Plotly.newPlot("myDiv", data, layout)
+var config = {
+  modeBarButtonsToRemove: ["autoScale2d", "autoscale", "editInChartStudio", "editinchartstudio", "hoverCompareCartesian", "hovercompare", "lasso", "lasso2d", "orbitRotation", "orbitrotation", "pan", "pan2d", "pan3d", "select", "select2d", "sendDataToCloud", "senddatatocloud", "tableRotation", "tablerotation", "toImage", "toggleHover", "toggleSpikelines", "togglehover", "togglespikelines", "toimage", "zoom", "zoom2d", "zoom3d", "zoomIn2d", "zoomInGeo", "zoomInMapbox", "zoomOut2d", "zoomOutGeo", "zoomOutMapbox"]}
+
+Plotly.newPlot("mygrahp", data, layout, config, {displaylogo: false}, {responsive: true})
